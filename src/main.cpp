9b271e2f9b02ed362026b91bd3f0922757199799
Ben Prisby <bdprisby@gmail.com>
2019-04-02 11:04:53 -0400

    Added command line options for specifying the server address and disabling NFC functionality.

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+#include <QCommandLineParser>
 #include <QDebug>
 #include <QFont>
 #include <QFontDatabase>
@@ -22,9 +23,32 @@ int main( int argc, char * argv[] )
     /* Bring in the Virtual Keybaord. */
     qputenv( "QT_IM_MODULE", QByteArray( "qtvirtualkeyboard" ) );
 
+    QCoreApplication::setApplicationName( "BCON Kiosk" );
+    QCoreApplication::setApplicationVersion( "1.0.0" );
+
     QCoreApplication::setAttribute( Qt::AA_EnableHighDpiScaling );
 
     QGuiApplication App( argc, argv );
+
+    /* Parse the command line arguments. */
+    QCommandLineParser Parser;
+    QCommandLineOption ServerOption( QStringList() << "s" << "server",
+                                     QCoreApplication::translate( "main", "Address of the backend server to connect to." ),
+                                     QCoreApplication::translate( "main", "IP:Port" ) );
+    QCommandLineOption NFCOption( QStringList() << "n" << "nonfc",
+                                  QCoreApplication::translate( "main", "Do not use NFC functionality." ) );
+    Parser.setApplicationDescription( "BCON Kiosk" );
+    Parser.addHelpOption();
+    Parser.addVersionOption();
+    Parser.addOption( ServerOption );
+    Parser.addOption( NFCOption );
+    Parser.process( App );
+
+    /* Check the command line arguments. */
+    QString sServerAddress = Parser.isSet( "server" ) ? Parser.value( "server" ) : "http://localhost:3000";
+    qDebug() << "Connecting to server at address" << sServerAddress;
+    bool bUseNFC = Parser.isSet( "nonfc" ) ? false : true;
+    qDebug() << ( bUseNFC ? "Enabling NFC functionality" : "Not using NFC functionality" );
 
     /* Set the custom font. */
     ( void )QFontDatabase::addApplicationFont( ":/fonts/San_Francisco-Bold.ttf" );
@@ -50,7 +74,7 @@ int main( int argc, char * argv[] )
     }
 
     /* Initialize the BCON network. */
-    pBackend = new BCONNetwork();
+    pBackend = new BCONNetwork( sServerAddress, bUseNFC );
 
     /* Request all games now to keep them in the data store for later reference. */
     pBackend->getAllGames();
